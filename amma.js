e5e6ae5318815ff3b92536053c2b585e8a2b3b62
ThanushaPasula <pasulathanusha@gmail.com>
2025-02-12 22:06:05 +0530

    Add files via upload
    amma.js

---
+++ amma.js
@@ -0,0 +1,113 @@
+// let arr = [21,31,41]
+// console.log(arr)
+// arr.unshift(31)
+// console.log(arr)
+
+
+
+// let array = [ 3 ,5,7,9]
+// console.log(array)
+// array.pop(9)
+// array.pop(5)
+// console.log(array)
+
+
+
+// let arr = [65,75,8,5]
+// console.log(arr.includes(1234))
+
+
+
+// function name(){
+//     console.log("function got run")
+// }
+// name()
+
+
+
+// function add (a,b){
+//     console.log(a+b)
+// }
+// add(1,9)
+
+
+
+
+// function add (a,b){
+//     console.log(a-b)
+// }
+// add(9,2)
+
+
+// let a=77
+// {
+//     let a=2
+// }
+// console.log(a)
+
+
+
+var a=77
+{
+    var a=2
+}
+console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
